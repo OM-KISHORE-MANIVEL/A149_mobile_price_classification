ddf696abfae3b1f80c30105bebc7cbc9c56973d3
AthulKrishnaRenjith <ar23agl@herts.ac.uk>
2024-11-10 14:26:51 +0000

    dependent variable histogram

--- Plot data.R
+++ Plot data.R
@@ -1,29 +1,46 @@
 #load data
 training<-read.csv("Data_csv/train.csv", header = TRUE)
 
+# histogram for all columns
 for (col_name in names(training)) {
   if (col_name != "mobile_wt") {
     
-    # histogram
     hist(training[[col_name]],
          main = paste("Histogram of", col_name),
          xlab = col_name,
-         col = "blue",
+         col = "lightblue",
          border = "black")
   }
 }
 
+# Dependent variable
+dependent_var <- training[["mobile_wt"]]
+
+# Histogram with normal curve
+hist(dependent_var,
+     main = "Histogram of mobile_wt with Normal Curve",
+     xlab = "mobile_wt",
+     col = "lightblue",
+     border = "black",
+     prob = TRUE)
+
+mean_val <- mean(dependent_var, na.rm = TRUE)
+sd_val <- sd(dependent_var, na.rm = TRUE)
+curve(dnorm(x, mean = mean_val, sd = sd_val),
+      col = "red", lwd = 2, add = TRUE)
+
+
+#plots
 for (col_name in names(training)) {
   if (col_name != "mobile_wt") {
     
-    #plots
     plot(training[["mobile_wt"]], training[[col_name]],
          xlab = "mobile_wt",
          ylab = col_name,
          main = paste("Plot of", col_name, "against mobile_wt"), type = "p", pch = 19,
          cex = 1.5, col = "black")
     
-    #linear trend line
+#linear trend line
     model <- lm(training[[col_name]] ~ training[["mobile_wt"]])
     abline(model, col = "blue", lwd = 2)
     
